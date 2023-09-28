c9dd21672fefb71ac01a0822f317310d1a7a7e1f
Satiel <mybeautifulwound@gmail.com>
2023-09-27 23:44:39 -0500

    enemy update
    added in enemies, using tweens for their intro-animation

---
+++ Enemy.gd
@@ -0,0 +1,56 @@
+extends Area2D
+
+signal died
+
+var start_pos = Vector2.ZERO
+var speed = 0
+
+@onready var screensize = get_viewport_rect().size
+
+func start(pos):
+	# set the enemy speed
+	speed = 0
+	
+	# set the enemy starting position
+	position = Vector2(pos.x, -pos.y)
+	start_pos = pos
+	
+	# wait until a random a mount of seconds before continuing movement
+	await get_tree().create_timer(randf_range(0.25, 0.55)).timeout
+	
+	# create the tween, which uses the 'trans_back' animation to go from the starting position to the top portion
+	var tween = create_tween().set_trans(Tween.TRANS_BACK)
+	tween.tween_property(self, "position:y", start_pos.y, 1.4)
+	
+	# wait until tween is finished
+	await tween.finished
+	$MoveTimer.wait_time = randf_range(5, 20)
+	$MoveTimer.start()
+	$ShootTimer.wait_time = randf_range(4, 20)
+	$ShootTimer.start()
+	
+	
+
+
+func _on_move_timer_timeout():
+	speed = randf_range(75, 100)
+
+
+func _on_shoot_timer_timeout():
+	$ShootTimer.wait_time = randf_range(4, 20)
+	$ShootTimer.start()
+	
+func _process(delta):
+	position.y += speed * delta
+	
+	# once the enemy leaves the map, start them back at the top
+	if position.y > screensize.y + 32:
+		start(start_pos)
+
+func explode():
+	speed = 0
+	$AnimationPlayer.play("Explode")
+	set_deferred("monitoring", false)
+	died.emit(5)
+	await $AnimationPlayer.animation_finished
+	queue_free()
